7015dc12098420436eb0c6d77cd8913fedc36180
Caroline <carolinefernandesc@hotmail.com>
2023-05-27 09:57:33 -0300

    Requisito 04: Finalizado lógica, pendente teste

--- backend/src/routers/products.router.js
+++ backend/src/routers/products.router.js
@@ -1,6 +1,7 @@
 const express = require('express');
 
 const { productsController } = require('../controllers');
+const validateProductName = require('../middlewares/validation.products');
 
 const router = express.Router();
 
@@ -8,6 +9,6 @@ router.get('/', productsController.getAll);
 
 router.get('/:id', productsController.getById);
 
-router.post('/', productsController.insert);
+router.post('/', validateProductName, productsController.insert);
 
 module.exports = router;
